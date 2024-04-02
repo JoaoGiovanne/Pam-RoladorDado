40d3aa9d8ca898954b992f984520ad007f55b774
JoaoGiovanne <124682607+JoaoGiovanne@users.noreply.github.com>
2024-04-02 12:40:02 -0300

    Arquivos Prova

---
+++ MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿namespace Prova
+{
+    public partial class MainPage : ContentPage
+    {
+        int dado = 0;
+        public MainPage()
+
+        {
+            InitializeComponent();
+        }
+
+        private void Rolar_Clicked(object sender, EventArgs e)
+        {
+            dado = Convert.ToInt32((string)picker.SelectedItem);
+            Random rnd = new Random();
+            int calc = rnd.Next(1, dado + 1) ;
+            visor.Text = calc.ToString();
+        }
+
+    }
+}
